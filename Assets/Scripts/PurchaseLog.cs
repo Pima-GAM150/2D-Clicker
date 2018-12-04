4f1066c92cd22b57ec917b516bbea89e2646cbd5
Isaak <imoore1@mail.pima.edu>
2018-12-04 02:45:40 -0700

    Created button that will be a purchasable upgrade that enables automine

---
+++ Assets/Scripts/PurchaseLog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchaseLog : MonoBehaviour {
+
+    public GameObject AutoMineOre;
+
+    public void StartAutoOre()
+    {
+        AutoMineOre.SetActive(true);
+    }
+}
