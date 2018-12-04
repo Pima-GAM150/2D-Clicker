7bde181d051784028d48aaa18211dba44778f08b
Isaak <imoore1@mail.pima.edu>
2018-12-04 01:54:37 -0700

    Added Text animation and if statement to prevent selling of negative ore

--- Assets/Scripts/Sell.cs
+++ Assets/Scripts/Sell.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Sell : MonoBehaviour
 {
     public GameObject textBox;
+    public GameObject statusBox;
 
     public void ClickTheButton()
     {
-        GlobalOre.OreCount -= 1;
-        GlobalCash.CashCount += 1;
+        if (GlobalOre.OreCount == 0)
+        {
+            statusBox.GetComponent<Text>().text = "Not enough ores to sell";
+            statusBox.GetComponent<Animation>().Play("StatusAnim");
+        }
+        else
+        {
+            GlobalOre.OreCount -= 1;
+            GlobalCash.CashCount += 1;
+        }
     }	
 	
 }
